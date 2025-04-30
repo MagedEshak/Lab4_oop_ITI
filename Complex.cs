3eec41ee7548f23a3b30986f06a152e6a9875c09
Maged Eshak <magedeshak36@gmail.com>
2025-04-30 14:33:49 +0300

    lab solution

---
+++ Complex.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_oop
+{
+    internal class Complex
+    {
+        int real;
+        int img;
+
+        public void SetReal(int _real)
+        {
+            this.real = _real;
+        }
+        public void SetImg(int _img)
+        {
+            this.img = _img;
+        }
+        public int GetReal()
+        {
+            return real;
+        }
+        public int GetImg()
+        {
+            return this.img;
+        }
+        public string PrintComplex()
+        {
+            return $"{this.real}+{img}i";
+        }
+
+        public Complex()
+        {
+            real=9;
+            img=4;
+        }
+
+
+        public Complex(int _real, int _img)
+        {
+            real = _real;
+            img = _img;
+        }
+
+        public Complex(int _number)
+        {
+            real = _number;
+            img = _number;
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            
+            Complex com1 = new Complex();
+            Complex com2 = new Complex(5, 6);
+            Complex com3 = new Complex(6);
+
+            Console.WriteLine(com1.PrintComplex());
+            Console.WriteLine(com2.PrintComplex());
+            Console.WriteLine(com3.PrintComplex());
+
+
+            //---------------------------------------------------------------
+            Console.WriteLine("================================================ ");
+
+            string reverse = "this is a test";
+
+            string[] wordReve = reverse.Split(' ');
+
+            for (int i = wordReve.Length - 1; i >= 0; i--)
+            {
+                Console.Write(wordReve[i] + " ");
+            }
+
+            //---------------------------------------------------------------
+            Console.WriteLine("\n================================================ ");
+
+            int[] arr = { 1, 2, 3, 4, 1, 5, 6, 1, 8, 9, 10, 1, 44, 55, 66, 77 };
+
+            int maxDis = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        int disc = j - i - 1;
+
+                        if (disc > maxDis)
+                        {
+                            maxDis = disc;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(maxDis);
         }
     }
 }
